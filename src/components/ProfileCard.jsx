745ff3ef3a2d0b497726aea197acf96a8b83f4e2
RohmatWorkstar <rohmatworkstar99@gmail.com>
2025-01-27 11:09:54 +0700

    fix deploy1

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -4,7 +4,7 @@ const ProfileCard = ({ profile }) => {
     // console.log(`profilecard:`, profile);
     return (
         <div className="bg-white w-50 h-50 lg:w-1/2 p-4">
-            <img src={profile.profile_image !== null ? profile.profile_image : "/src/assets/Profile Photo.png"} className="mb-5" alt="Profile" width={120} />
+            <img src={profile.profile_image !== null ? profile.profile_image : "/assets/Profile Photo.png"} className="mb-5" alt="Profile" width={120} />
             <p>Selamat Datang,</p>
             <h1 className="text-4xl font-bold mb-4">{profile.first_name}</h1>
         </div>
